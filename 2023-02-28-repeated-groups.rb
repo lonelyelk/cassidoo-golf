65e47e9b081dbcf11438e3935d4e54edbf9f6a72
Sergey Kruk <sergey.kruk@gmail.com>
2023-02-28 20:26:41 +0100

    Add the same length solution using filter_map

--- 2023-02-28-repeated-groups.rb
+++ 2023-02-28-repeated-groups.rb
@@ -1,6 +1,7 @@
 # https://buttondown.email/cassidoo/archive/like-what-you-do-and-then-you-will-do-your-best/
 
 f=->a{a.chunk{_1}.map{_2[1]&&_2}-[nil]}
+f=->a{a.chunk{_1}.filter_map{_2[1]&&_2}}
 
 p f[[1, 2, 2, 4, 5]] # [[2, 2]]
 p f[[1, 1, 0, 0, 8, 4, 4, 4, 3, 2, 1, 9, 9]] # [[1, 1], [0, 0], [4, 4, 4], [9, 9]]
