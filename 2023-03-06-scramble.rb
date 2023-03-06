b699e0b722f3fc4ace9222c49a4c2f31d62d5dff
Sergey Kruk <sergey.kruk@gmail.com>
2023-03-06 23:54:45 +0100

    Add link to the letter

--- 2023-03-06-scramble.rb
+++ 2023-03-06-scramble.rb
@@ -1,3 +1,5 @@
+# https://buttondown.email/cassidoo/archive/you-should-celebrate-who-you-are-eva-mendes/
+
 f=->s{s.gsub(/(?<=\w)\w{2,}(?=\w)/){*c=d=_1.chars;loop{break(d*'')if c!=d.shuffle!}}}
 
 p f["A quick brown fox jumped over the lazy dog."]
