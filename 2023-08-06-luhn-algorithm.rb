c20a2083563628198448d70043bf54ecd5da7d11
Sergey Kruk <sergey.kruk@gmail.com>
2023-09-10 00:37:54 +0200

    Make Luhn algorithm shorter

--- 2023-08-06-luhn-algorithm.rb
+++ 2023-08-06-luhn-algorithm.rb
@@ -3,6 +3,7 @@
 # https://buttondown.email/cassidoo/archive/it-isnt-the-mountains-ahead-to-climb-that-wear/
 
 f=->n{c,*a=n.digits;10-a.sum{(_1*((n=!n)?1:2)).digits.sum}%10==c}
+f=->n{c,*a=n.digits;10-a.sum{_1.*((n=!n)?1:2).digits.sum}%10==c}
 
 p f[123456789] # > false
 p f[5555555555554444] # > true // Mastercard
