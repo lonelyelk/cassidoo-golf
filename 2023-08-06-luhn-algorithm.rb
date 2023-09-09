459d8c8d5868393cdc86ee4ecddf6ebb377a894d
Sergey Kruk <sergey.kruk@gmail.com>
2023-09-10 00:22:11 +0200

    Add Luhn algorithm

---
+++ 2023-08-06-luhn-algorithm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# https://buttondown.email/cassidoo/archive/it-isnt-the-mountains-ahead-to-climb-that-wear/
+
+f=->n{c,*a=n.digits;10-a.sum{(_1*((n=!n)?1:2)).digits.sum}%10==c}
+
+p f[123456789] # > false
+p f[5555555555554444] # > true // Mastercard
