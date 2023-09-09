f5103bb33bd753619dff792d0babbbd373720109
Sergey Kruk <sergey.kruk@gmail.com>
2023-09-09 22:28:44 +0200

    Make missing letters shorter

--- 2023-06-25-missing-letters.rb
+++ 2023-06-25-missing-letters.rb
@@ -2,7 +2,7 @@
 
 # https://buttondown.email/cassidoo/archive/change-is-a-stranger-you-have-yet-to-know-george/
 
-f=->a{(a[0]..a[-1]).to_a-a}
+f=->a{[*a[0]..a[-1]]-a}
 
 p f[['a','b','c','d','f']] # > ['e']
 p f[['a','b','c','d','e','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z']] # > ['f','g','v']
