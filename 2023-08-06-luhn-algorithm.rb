a149073bb3c1cc56e4aedbf6538b0c464588acf2
Sergey Kruk <sergey.kruk@gmail.com>
2023-09-10 12:06:40 +0200

    Make Luhn algorithm shorter

--- 2023-08-06-luhn-algorithm.rb
+++ 2023-08-06-luhn-algorithm.rb
@@ -2,7 +2,6 @@
 
 # https://buttondown.email/cassidoo/archive/it-isnt-the-mountains-ahead-to-climb-that-wear/
 
-f=->n{c,*a=n.digits;10-a.sum{(_1*((n=!n)?1:2)).digits.sum}%10==c}
 f=->n{c,*a=n.digits;10-a.sum{_1.*((n=!n)?1:2).digits.sum}%10==c}
 
 p f[123456789] # > false
