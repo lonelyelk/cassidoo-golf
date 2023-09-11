5b5d05326776cea03d950e2ec772e80b7386970b
Sergey Kruk <sergey.kruk@gmail.com>
2023-09-11 23:54:27 +0200

    Add guessing game

---
+++ 2023-08-20-guessing-game.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# https://buttondown.email/cassidoo/archive/not-everything-that-is-faced-can-be-changed-but-1047/
+
+f=->{n=rand 100;i=0;puts($_.to_i>n ??v:?^)while(i+=1)&&n!=gets.to_i;i}
+
+p f[]
