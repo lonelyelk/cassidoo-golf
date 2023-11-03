6782fa7bf4be6cc9f98e01dd576570a9d49cba2f
Sergey Kruk <sergey.kruk@gmail.com>
2023-11-03 13:30:47 +0100

    Remove rescue, move comment

--- 2023-10-02-add-operators.rb
+++ 2023-10-02-add-operators.rb
@@ -4,10 +4,11 @@
 
 # This is O(n^m) where m = number of possible operators + 1. In my case it's O(n^5),
 # but if we only keep + and * it sill be O(n^3). Assuming of course, that sticking digits is a possibility.
-# Removing division can also reduce the number of bytes as we no longer need to convert to float.
 f=->(n,r){d=n.digits;e=->(a,x){x[0]?['',?+,?*,?-,?/].flat_map{e[a+_1+x[-1].to_s,x[..-2]]}.select{eval(_1.sub(/(\D)/,'.0\1'))==r rescue nil}:a};e[d.pop.to_s,d]}
 
-f_no_float=->(n,r){d=n.digits;e=->(a,x){x[0]?['',?+,?*,?-].flat_map{e[a+_1+x[-1].to_s,x[..-2]]}.select{eval(_1)==r rescue nil}:a};e[d.pop.to_s,d]}
+# Removing division can also reduce the number of bytes as we no longer need to convert to float.
+# Also no need to rescue division by 0.
+f_no_float=->(n,r){d=n.digits;e=->(a,x){x[0]?['',?+,?*,?-].flat_map{e[a+_1+x[-1].to_s,x[..-2]]}.select{eval(_1)==r}:a};e[d.pop.to_s,d]}
 
 p f[123, 6] # > ["1*2*3", "1+2+3"]
 p f_no_float[123, 6] # > ["1*2*3", "1+2+3"]
